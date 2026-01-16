d767c95e6d118d7a32378967c38fceda5bbc3b20
Gasym Valiyev <outnesss@yandex.ru>
2026-01-17 00:48:47 +0400

    Resolved dependency handling in scripts!

--- Build LLM From Scratch/From-Scratch/launch_pretrain.py
+++ Build LLM From Scratch/From-Scratch/launch_pretrain.py
@@ -1,13 +1,48 @@
+# "E:\Apps\python_envs\cuda_gpu_env\python.exe" launch_pretrain.py
+
 import torch
-import torch.nn as nn
+import tiktoken
+from text_and_attention import create_dataloader_v1
 from llm_architecture import GPTModel
 from gpt_config import GPT_CONFIG_124M
-from utility import train_model_simple, evaluate_model, generate_and_print_sample
-
+from utility import train_model_simple
 
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+file_path = "the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+
 optimizer = torch.optim.AdamW(
      model.parameters(),           #1 The .parameters() method returns all trainable weight parameters of the model. 
     lr=0.0004, weight_decay=0.1
--- Build LLM From Scratch/From-Scratch/llm_architecture.py
+++ Build LLM From Scratch/From-Scratch/llm_architecture.py
@@ -3,17 +3,6 @@ import torch.nn as nn
 from text_and_attention import MultiHeadAttention
 
 
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,     # Vocabulary size
-    "context_length": 1024,  # Context length
-    "emb_dim": 768,          # Embedding dimension
-    "n_heads": 12,           # Number of attention heads
-    "n_layers": 12,          # Number of layers
-    "drop_rate": 0.1,        # Dropout rate
-    "qkv_bias": False        # Query-Key-Value bias
-}
-
-
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
