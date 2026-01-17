9febf6ed3faaa4fd6df4d99c5929ffed5ed2e427
Gasym Valiyev <outnesss@yandex.ru>
2026-01-18 03:29:52 +0400

    Added top-k / top-p sampling notes & some code line comments.

--- Build LLM From Scratch/From-Scratch/decoding.py
+++ Build LLM From Scratch/From-Scratch/decoding.py
@@ -8,12 +8,12 @@ def softmax_with_temperature(logits, temperature):
 
 def generate(model, idx, max_new_tokens, context_size,
              temperature=0.0, top_k=None, eos_id=None):
-    for _ in range(max_new_tokens):
+    for _ in range(max_new_tokens): #1 The for loop: gets logits and only focuses on the last time step.
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]
-        if top_k is not None:
+        if top_k is not None: #2 Filters logits with top_k sampling 
             top_logits, _ = torch.topk(logits, top_k)
             min_val = top_logits[:, -1]
             logits = torch.where(
@@ -21,13 +21,13 @@ def generate(model, idx, max_new_tokens, context_size,
                 torch.tensor(float("-inf")).to(logits.device),
                 logits
             )
-        if temperature > 0.0:
+        if temperature > 0.0: #3 Applies temperature scaling
             logits = logits / temperature
             probs = torch.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
-        else:
+        else: #4 Carries out greedy next-token selection as before when temperature scaling is disabled 
             idx_next = torch.argmax(logits, dim=-1, keepdim=True)
-        if idx_next == eos_id:
+        if idx_next == eos_id: #5 Stops generating early if end-of-sequence token is encountered
             break
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
