1d76a88c89408eb2cb24d1e96331c051db1f60fb
Gasym Valiyev <outnesss@yandex.ru>
2026-02-18 22:24:45 +0400

    Added ollama_utilities.py

---
+++ Build LLM From Scratch/Supervised-Instruction-Fine-Tuning/ollama_utilities.py
@@ -0,0 +1,53 @@
+import psutil
+import json
+import urllib.request
+
+
+def check_if_running(process_name):
+    running = False
+    for proc in psutil.process_iter(["name"]):
+        if process_name in proc.info["name"]:
+            running = True
+            break
+    return running
+
+
+def query_model(
+    prompt, 
+    #model="llama3", 
+    model="gemma3:1b",
+    url="http://localhost:11434/api/chat"
+):
+    data = {             #1 Creates the data payload as a dictionary 
+        "model": model,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ],
+        "options": {         #2 Settings for deterministic responses 
+            "seed": 123,
+            "temperature": 0,
+            "num_ctx": 2048
+        }
+    }
+
+    payload = json.dumps(data).encode("utf-8")    #3 Converts the dictionary to a JSON-formatted string and encodes it to bytes 
+    request = urllib.request.Request(#4 Creates a request object, setting the method to POST and adding necessary headers 
+        url,                                                #4
+        data=payload,                                       #4
+        method="POST"                                       #4
+    ) #4
+
+    request.add_header("Content-Type", "application/json")   #4
+
+    response_data = ""
+    with urllib.request.urlopen(request) as response:   #5 Sends the request and captures the response 
+        while True:
+            line = response.readline().decode("utf-8")
+            if not line:
+                break
+            response_json = json.loads(line)
+            response_data += response_json["message"]["content"]
+
+    return response_data
+
+
